7f4c9d746d689692498914a16b3cc10360d38059
Ryan <ryan.lim.2024@computing.smu.edu.sg>
2024-12-10 14:39:23 +0800

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly right now.</li>
+          <li>My name is eugene.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
